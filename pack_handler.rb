7ee659c98ac7455db20a900609f4c143b976068d
oleg <krasno1986@gmail.com>
2020-07-31 16:28:04 +0300

    add method

--- pack_handler.rb
+++ pack_handler.rb
@@ -1,28 +1,37 @@
-UNIQ_ARRAY = [1,2,3]
+$uniq_arr = [1,2,3]
 
 class PackHandler
   # class << self
-  #   attr_accessor​ :elements
+  # attr_accessor​ :elements
   # end
 
-  @elements = []
+  # @elements = []
 
-  # def ​initialize()
+  # def ​initialize(elements = [])
+  #   @elements = elements
   # end
+
+  def elements=(val=[])
+    @elements = val
+  end
 
   def proc_items arr
     arr.each do |e|
-      yield(e) unless UNIQ_ARRAY.include?(e)
+      yield(e) unless procd_items.include?(e)
     end
   end
 
-  # def reset
-  #   UNIQ_ARRAY = []
-  # end
+  def reset
+    $uniq_arr = []
+  end
+
+  def procd_items
+    $uniq_arr
+  end
 end
 
 v = PackHandler.new
 v.proc_items([3,4,5]) do |e|
-  UNIQ_ARRAY << e
+  $uniq_arr << e
 end
-p UNIQ_ARRAY
+p v.procd_items
