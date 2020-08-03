d7c8c98925afc0e9ab4a2872db8e1977e17400bb
oleg <krasno1986@gmail.com>
2020-08-03 11:59:54 +0300

    add checker

--- pack_handler.rb
+++ pack_handler.rb
@@ -1,23 +1,17 @@
 $uniq_arr = [1,2,3]
 
 class PackHandler
-  # class << self
-  # attr_accessor​ :elements
-  # end
-
-  # @elements = []
-
-  # def ​initialize(elements = [])
-  #   @elements = elements
-  # end
-
-  def elements=(val=[])
-    @elements = val
+  def key=(val=nil)
+    @key = val
   end
 
   def proc_items arr
+    unless @key.nil? && (@key.class == Symbol || String)
+      arr.uniq!{|e| e[@key]}
+    end
+
     arr.each do |e|
-      yield(e) unless procd_items.include?(e)
+      yield(e) unless $uniq_arr.include?(e)
     end
   end
 
@@ -28,10 +22,35 @@ class PackHandler
   def procd_items
     $uniq_arr
   end
+
+  def identify arg
+    @key = arg
+    @key
+  end
 end
 
+array_of_hashes = [{id: 1}, {id: 1, test_key: 'Some data'}, {id: 2}]
+array_of_hashes2 = [{id: 2}, {id: 3}]
 v = PackHandler.new
 v.proc_items([3,4,5]) do |e|
   $uniq_arr << e
 end
-p v.procd_items
+
+p $uniq_arr
+v.procd_items
+v.reset
+v.identify :id
+
+v.proc_items(array_of_hashes) do |e|
+  $uniq_arr << e
+end
+
+v.proc_items(array_of_hashes2) do |e|
+  # if e.class == Hash
+  #   array_of_hashes.each{|hash| hash if hash. }
+  # end
+  # array_of_hashes = array_of_hashes.uniq{|e| e[key]}
+  $uniq_arr << e
+end
+
+p $uniq_arr
