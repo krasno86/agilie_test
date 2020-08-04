51d316688d7067ea9970c410d560d27afc49250d
oleg <krasno1986@gmail.com>
2020-08-04 10:47:47 +0300

    add class checker

--- pack_handler.rb
+++ pack_handler.rb
@@ -1,71 +1,108 @@
 class PackHandler
-  attr_accessor :arr, :key
+  attr_accessor :processed_elements, :resulted_array, :uniq_by
 
-  def initialize(arr = [], key = nil)
-    @arr = arr
-    @key = key
+  def initialize
+    @processed_elements = []
+    @resulted_array = []
+    @uniq_by = ''
   end
 
-  def proc_items args
-    unless @key.nil? && (@key.class == Symbol || String)
-      args.uniq!{|e| e[@key]}
-    end
+  def proc_items arr
+    arr.each do |arg|
+      next if can_be_processed?(arg)
 
-    args.each do |e|
-      yield(e) unless element_processed?(e) #&& should_proc?
+      self.processed_elements << arg
+      self.resulted_array << yield(arg)
     end
   end
 
   def reset
-    self.arr = []
+    self.processed_elements = []
+    self.resulted_array = []
+    self.uniq_by = ''
   end
 
   def procd_items
-    self.arr
+    self.resulted_array
   end
 
-  def identify arg
-    self.key = arg
+  def identify key
+    self.uniq_by = key
   end
 
-  # def should_proc?
-  #   yield(arg)
-  # end
+  private
 
-  def element_processed? element
-    self.arr.include?(element)
+  def can_be_processed?(element)
+    case
+      when element.respond_to?(uniq_by)
+        processed_elements.any? { |item| item.send(uniq_by) == element.send(uniq_by) }
+      when element.is_a?(Hash)
+        processed_elements.any? { |item| item[uniq_by] == element[uniq_by] }
+      else
+        processed_elements.include?(element)
+    end
   end
 end
 
-v = PackHandler.new([1,2,3])
+v = PackHandler.new
+v.proc_items([1,2,3]) do |e|
+  e
+end
+
 v.proc_items([3,4,5]) do |e|
-  v.arr << e
+  e
 end
 
-p v.arr
+p v.resulted_array
 v.procd_items
 v.reset
 v.identify :id
 
 v.proc_items([{id: 1}, {id: 1, test_key: 'Some data'}, {id: 2}]) do |e|
-  v.arr << e
+  e
 end
 
 v.proc_items([{id: 2}, {id: 3}]) do |e|
-  v.arr << e
+  e
 end
 
-p v.arr
+p v.resulted_array
 
 v.reset
 v.identify :value
 
-# v.should_proc?​ do |item|
+
+v.proc_items([{value: 2}, {value: 3}]) do |e|
+  e
+end
+
+# v.should_proc​ do |item|
 #   item[:value​] % 2 == 0
 # end
 
 v.proc_items([{value: 2}, {value: 3}]) do |e|
-  v.arr << e if e[v.key] % 2 == 0
+  e
 end
 
-p v.arr
+class SomeClass
+  attr_accessor :main_field
+
+  def initialize(main_field = nil)
+    @main_field = main_field
+  end
+end
+
+a = SomeClass.new('a')
+b = SomeClass.new('b')
+
+p_h = PackHandler.new
+p_h.identify :main_field
+p_h.proc_items([a, b]) do |item|
+  item
+end
+
+p_h.proc_items([SomeClass.new('a')]) do |item|
+  item
+end
+
+p p_h.resulted_array
